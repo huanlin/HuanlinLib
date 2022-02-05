52cc973dc83bb2e332aba3d15012e644148ec8bd
Michael Tsai <huanlin.tsai@gmail.com>
2022-02-05 23:39:52 +0800

    Added AppVeyor config.

--- build/BuildScript.cs
+++ build/BuildScript.cs
@@ -4,18 +4,10 @@ using FlubuCore.Context.Attributes.BuildProperties;
 using FlubuCore.IO;
 using FlubuCore.Scripting;
 
-/* 
- * 此範例展示 FlubuCore 搭配 MinVer 來指定應用程式的版本編號。已使用 FlubuCore v6.3.2 測試過。
- * 做法很簡單，只要把 MinVer 套件加入應用程式專案就行了，然後用 git tag 命令來指定應用程式的版本。
- * 除此之外，不用加入其他任何設定，建置腳本裡面也不用寫任何與版本編號有關的程式碼。
- */
 namespace Build
 {
     public class BuildScript : DefaultBuildScript
     {
-        // 指定建置結果的輸出目錄。
-        public FullPath OutputDirectory => RootDirectory.CombineWith("output");
-
         [ProductId]
         public string ProductId { get; set; } = "HuanlinLib";
 
@@ -40,8 +32,7 @@ namespace Build
             var publish = session.CreateTarget("pack")
                 .SetDescription("建立 NuGet 套件。")
                 .DependsOn(compile)
-                .AddCoreTask(x => x.Publish()
-                    .OutputDirectory(OutputDirectory));
+                .AddCoreTask(x => x.Pack());
         }
     }
 }
