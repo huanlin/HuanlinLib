201011ec7008313d182124f2f03bf2c4c3df9867
huanlin <huanlin.tsai@gmail.com>
2025-06-28 13:02:24 +0800

    把測試專案納入 Huanlin.Lib solution 一起管理。

---
+++ src/Test.Huanlin/ByteHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Test.Huanlin
+{
+    /// <summary>
+    ///This is a test class for Huanlin.Common.Helpers.ByteHelper and is intended
+    ///to contain all Huanlin.Common.Helpers.ByteHelper Unit Tests
+    ///</summary>
+    [TestFixture]
+    public class ByteHelperTest
+    {
+        /// <summary>
+        ///A test for Reverse (byte)
+        ///</summary>
+        [Test]
+        public void ReverseTest()
+        {
+            byte aByte = 0xb8;
+            byte expected = 0x1d;
+            byte actual = global::Huanlin.Common.Helpers.ByteHelper.Reverse(aByte);
+            Assert.That(actual, Is.EqualTo(expected), "Huanlin.Common.Helpers.ByteHelper.Reverse 測試失敗!");
+
+            aByte = 0x93;
+            expected = 0xc9;
+            actual = global::Huanlin.Common.Helpers.ByteHelper.Reverse(aByte);
+            Assert.That(actual, Is.EqualTo(expected), "Huanlin.Common.Helpers.ByteHelper.Reverse 測試失敗!");
+        }
+    }
+
+
+}
